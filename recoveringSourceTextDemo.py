2d85438437a0c41248022cba1c1fbc6853d4d773
forman.simon <forman.simon@72037544-6e44-0410-b90f-618e091ae1b3>
2008-01-30 00:44:41 +0000

    Some cleanup and new demos.  Made the parser iterable, at the cost of some processing time,  Don't make multi-thousand widget GUIs with this parser.

---
+++ recoveringSourceTextDemo.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+'''
+This script shows how to access the AST node of an ElementTree object,
+then the Tokens that contributed to that AST, and finally the original
+text of the ET's spec in the source string.
+'''
+from pygoo import Scanner, Parser, Formatter
+
+
+source = '''
+
+text
+    width = 20
+    height = 10
+    sticky = nsew
+    row = 1
+    rowspan = 2
+.
+
+button
+    text = plot
+    <Button-1> = plotCallback
+    sticky = nsew
+    row = 1
+    column = 1
+.
+
+button
+    text = reset
+    sticky = nsew
+    row = 2
+    column = 1
+.
+
+label
+    text = "A Simple Demo Widget"
+    sticky = nsew
+    row = 0
+    columnspan = 2
+.
+'''
+
+
+asts = Parser().parse(Scanner().tokenize(source))
+
+
+macros = asts.next()
+ets = Formatter(macros).convert(asts)
+
+for et in ets:
+
+    # tokens attribute is a set.
+    tokens = sorted(et.ast.tokens, key=lambda tok: tok.begin)
+
+    begin, end = tokens[0].begin, tokens[-1].end
+    print source[begin:end]
+    print
+
+    for token in tokens:
+        print source[token.begin:token.end],
+    print
+    print
--- simpleDemo.py
+++ simpleDemo.py
@@ -1,60 +1,59 @@
 #!/usr/bin/env python
-from Tkinter import Tk, Toplevel, Frame
+'''
+A simple demo that shows how to write a function that will produce new
+"Toplevel" windows on the fly from a passed in source string.
+'''
+from Tkinter import Tk, Toplevel
 from pygoo import toXML, realize
 
 
 source = '''
-
-text
-    width = 20
-    height = 10
-    sticky = nsew
-    row = 1
-    rowspan = 2
-.
-
-button
-    text = plot
-    <Button-1> = plotCallback
-    sticky = nsew
-    row = 1
-    column = 1
-.
-
-button
-    text = reset
-    sticky = nsew
-    row = 2
-    column = 1
-.
-
-label
-    text = "A Simple Demo Widget"
-    sticky = nsew
-    row = 0
-    columnspan = 2
-.
-'''
+    button
+        text = "Click Me!"
+        <Button-1> = callback
+        sticky = nsew
+        row = 1
+    .
+    button
+        text = "I do nothing"
+        sticky = nsew
+        row = 1
+        column = 1
+    .
+    label
+        text = "A Simple Demo Widget"
+        sticky = nsew
+        row = 0
+        columnspan = 2
+    .'''
 
 
-def windowFromSource(source, parent=None, namespace=None):
-    if parent is None:
-        top = Tk()
+def text2window(source, title, root=None, namespace=None):
+    '''
+    Convert source to a new Toplevel window using root if given.  The
+    namespace is passed to the realize() function.
+    '''
+    if root is not None:
+        # Create a new Toplevel from the passed in root.
+        top = Toplevel(root)
     else:
-        top = Toplevel(parent)
-    F = Frame(top)
-    F.grid(sticky='nsew')
+        # Create a brand new Tk interpreter-with-Toplevel.
+        top = Tk()
+
+    top.title(title)
+
+    # Realize the widgets.
     for widget in toXML(source):
-        realize(F, widget, d)
+        realize(top, widget, namespace)
+
     return top
 
 
-def plotCallback(event):
+def callback(event):
     print event
+    print repr(event.widget)
+    print '----------'
 
 
-d = {'plotCallback': plotCallback}
-widget = windowFromSource(source, namespace=d)
-print d
-
-widget.mainloop()
+window = text2window(source, "Pygoo Demo", namespace={'callback':callback})
+window.mainloop()
